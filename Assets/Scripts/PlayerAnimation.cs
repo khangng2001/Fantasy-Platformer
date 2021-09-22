e21b8c562aa162086cf5fe1ac2cfa0e7891870dd
khangng2001 <72006373+khangng2001@users.noreply.github.com>
2021-09-22 16:39:35 +0700

    player movement

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator anim;
+    private static string RUN_ANIMATION_TAG = "Run";
+    private static string JUMP_ANIMATION_TAG = "Jump";
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    public void RunningAnimation(bool run){
+        anim.SetBool(RUN_ANIMATION_TAG,run);
+    }
+    public void JumpingAnimation(bool jump){
+        anim.SetBool(JUMP_ANIMATION_TAG, jump);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D playerBody;
+    private float moveSpeed = 5f,jumpForce=4f;
+    private BoxCollider2D boxCollider2D;
+    [SerializeField] private LayerMask groundLayer;
+    private PlayerAnimation playerAnimation;
+    private Vector3 temPos;
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody2D>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+        playerAnimation = GetComponent<PlayerAnimation>();
+    }
+
+    private void Update() {
+        HandleMovement();
+        HandleAnimation();
+    }
+    
+    private void FixedUpdate() {
+        HandleJumping();
+    }
+    private void HandleMovement(){
+         temPos = transform.position;
+        if(Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.LeftArrow))
+        {
+            temPos.x -= moveSpeed * Time.deltaTime;
+            playerAnimation.RunningAnimation(true);
+            playerBody.transform.localScale = new Vector3(-1f, 1f, 1f);
+        }else if(Input.GetKey(KeyCode.D)||Input.GetKey(KeyCode.RightArrow))
+        {
+            temPos.x += moveSpeed * Time.deltaTime;
+            playerBody.transform.localScale = Vector3.one;
+            playerAnimation.RunningAnimation(true);
+        }
+        else{
+             playerAnimation.RunningAnimation(false);
+        }
+        transform.position = temPos;
+    }
+    private void HandleJumping(){
+        if(Input.GetKey(KeyCode.Space)){
+            if (IsGround()){
+                playerBody.velocity = new Vector2(playerBody.velocity.x, jumpForce);
+            }
+        }
+    }
+    private bool IsGround(){
+        return Physics2D.BoxCast(transform.position, boxCollider2D.bounds.size, 0f, Vector2.down,0.1f,groundLayer);
+    }
+    private void HandleAnimation(){
+        playerAnimation.JumpingAnimation(!IsGround());
+    }
+
+}
